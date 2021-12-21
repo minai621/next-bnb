78c3eca065f59d7acb9dcdb49f38c1acf956ef9f
minai621 <minai621@naver.com>
2021-12-22 03:15:44 +0900

    RegisterRoomBedroomType

--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -1,22 +1,64 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RegisterRoomState } from '../types/reduxState';
+import { BedType } from '../types/room';
 
-type RegisterRoomState = {
-    largeBuildingType: string | null;
-    buildingType: string | null;
-    roomType: string | null;
-    isSetUpForGuest: boolean | null;
-}
-
+//* 초기 상태
 const initialState: RegisterRoomState = {
-    // 건물 유형 큰 범주
+    //* 건물유형 큰 범주
     largeBuildingType: null,
-    // 건물 유형
+    //* 건물유형
     buildingType: null,
-    // 숙소 유형
+    //* 숙소유형
     roomType: null,
-    // 게스트를 위한 숙소
-    isSetUpForGuest: null
-};
+    //* 게스트만을 위해 만들어진 숙소인가
+    isSetUpForGuest: null,
+    //* 최대 숙박 인원
+    maximumGuestCount: 1,
+    //* 침실 개수
+    bedroomCount: 0,
+    //* 침대 개수
+    bedCount: 1,
+    //* 침대 유형
+    bedList: [],
+    //* 공용공간 침대 유형
+    publicBedList: [],
+    //* 욕실 개수
+    bathroomCount: 1,
+    //* 욕실 유형
+    bathroomType: null,
+    //* 국가/지역
+    country: '',
+    //* 시/도
+    city: '',
+    //* 시/군/구
+    district: '',
+    //* 도로명주소
+    streetAddress: '',
+    //* 동호수
+    detailAddress: '',
+    //* 우편번호
+    postcode: '',
+    //* 위도
+    latitude: 0,
+    //* 경도
+    longitude: 0,
+    //* 편의시설
+    amentities: [],
+    //* 편의공간
+    conveniences: [],
+    //* 편의공간
+    photos: [],
+    //* 숙소 설명
+    description: '',
+    //* 숙소 제목
+    title: '',
+    //* 숙소 요금
+    price: 0,
+    //* 예약 시작 날짜
+    startDate: null,
+    //* 예약 마감 날짜
+    endDate: null,
+  };
 
 const registerRoom = createSlice({
     name: 'registerRoom',
@@ -44,6 +86,73 @@ const registerRoom = createSlice({
             state.isSetUpForGuest = action.payload;
             return state;
         },
+        setMaximumGuestCount(state, action: PayloadAction<number>) {
+            state.maximumGuestCount = action.payload;
+            return state;
+        },
+        setBedroomCount(state, action: PayloadAction<number>) {
+            const bedroomCount = action.payload;
+            let { bedList } = state;
+
+            state.bedroomCount = bedroomCount;
+
+            if (bedroomCount < bedList.length) {
+                bedList = state.bedList.slice(0, bedroomCount);
+            } else {
+                for (let i = bedList.length + 1; i < bedroomCount + 1; i += 1) {
+                    bedList.push({ id: i, beds: [] });
+                }
+            }
+            state.bedList = bedList;
+            return state;
+        },
+        setBedCount(state, action: PayloadAction<number>) {
+            state.bedCount = action.payload;
+            return state;
+        },
+        setBedTypeCount(
+            state,
+            action: PayloadAction<{ bedroomId: number; type: BedType; count: number }>
+          ) {
+            const { bedroomId, type, count } = action.payload;
+
+            const bedroom = state.bedList[bedroomId - 1];
+
+            const prevBeds = bedroom.beds;
+            const index = prevBeds.findIndex((bed) => bed.type === type);
+            if (index === -1) {
+              //* 타입이 없다면
+              state.bedList[bedroomId - 1].beds = [...prevBeds, { type, count }];
+              return state;
+            }
+            //* 타입이 존재한다면
+            if (count === 0) {
+              state.bedList[bedroomId - 1].beds.splice(index, 1);
+            } else {
+              state.bedList[bedroomId - 1].beds[index].count = count;
+            }
+            return state;
+          },
+          setPublicBedTypeCount(
+            state,
+            action: PayloadAction<{ type: BedType; count: number }>
+          ) {
+            const { type, count } = action.payload;
+
+            const index = state.publicBedList.findIndex((bed) => bed.type === type);
+            if (index === -1) {
+              //* 타입이 없다면
+              state.publicBedList = [...state.publicBedList, { type, count }];
+              return state;
+            }
+            //* 타입이 존재한다면
+            if (count === 0) {
+              state.publicBedList.splice(index, 1);
+            } else {
+              state.publicBedList[index].count = count;
+            }
+            return state;
+          },
     }
 });
 
